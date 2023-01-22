d6802e763ae7d750897478012e376ba97f93d5c2
Julian-Lop <julian01andres@gmail.com>
2023-01-22 15:21:51 -0500

    store implemented

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,13 @@
+import { Store } from '@/utils/Store'
 import Head from 'next/head'
 import Link from 'next/link'
-import React from 'react'
+import React, { useContext } from 'react'
 
 export default function Layout({title, children}) {
+
+    const {state, dispatch} = useContext(Store)
+    const {cart} = state
+
   return (
     <>      
             <Head>
@@ -20,7 +25,13 @@ export default function Layout({title, children}) {
                         </Link>
                         <div className='font-semibold'>
                             <Link legacyBehavior href='/'>
-                                <a className='p-2'>Cart</a>
+                                <a className='p-2'>Cart
+                                {cart.cartItems.length > 0 && (
+                                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
+                                        {cart.cartItems.reduce((a,c) => a + c.quantity, 0)}
+                                    </span>
+                                )}
+                                </a>
                             </Link>
                             <Link legacyBehavior href='/'>
                                 <a className='p-2'>Login</a>
--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,11 +1,13 @@
 import Layout from '@/components/Layout'
 import data from '@/utils/data'
+import { Store } from '@/utils/Store'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useContext } from 'react'
 
 export default function ProductScreen() {
+    const {state, dispatch } = useContext(Store) 
 
     const {query: {slug}} = useRouter()
 
@@ -16,6 +18,19 @@ export default function ProductScreen() {
             Product not found
         </div>
     }
+
+    const addToCartHandler = () => {
+        const existItem = state.cart.cartItems.find((x) => x.slug === product.slug)
+        const quantity = existItem ? existItem.quantity + 1 : 1
+        
+        if(product.countInStock < quantity){
+            alert('Sorry, Product is out of stock')
+            return 
+        }
+        
+        dispatch({type:'CART_ADD_ITEM', payload: {...product, quantity: quantity}})
+    }
+
     return (
         <Layout title={product.name}>
             <div className='py-2 font-semibold'>
@@ -54,7 +69,7 @@ export default function ProductScreen() {
                             <div>Status</div>
                             <div>{product.countInStock > 0 ? 'In Stock' : 'Unavailable'}</div>
                         </div>
-                        <button className='primary-button w-full' type="button">Add to cart</button>
+                        <button className='primary-button w-full' type="button" onClick={addToCartHandler}>Add to cart</button>
                     </div>
                 </div>
             </div>
